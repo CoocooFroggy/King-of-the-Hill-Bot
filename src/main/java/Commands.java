03fa249c630e2f0e8a99f655f3c57383a086be72
John Klay <johnandklay@gmail.com>
2021-02-04 13:15:10 -0500

    Initial commit

---
+++ src/main/java/Commands.java
@@ -0,0 +1,161 @@
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.*;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
+public class Commands {
+    public static void pushCommand(User user, Member member, Guild guild, TextChannel channel, List<User> mentionedUsers) throws SQLException {
+        //Vars
+        Statement statement = Main.statement;
+        String userId = user.getId();
+        String guildId = guild.getId();
+        String channelId = channel.getId();
+        String nickname;
+
+        //Get personal name for user
+        if (member.getNickname() == null)
+            nickname = user.getName();
+        else
+            nickname = member.getNickname();
+
+        //Get user ID of king
+        ResultSet resultSet = statement.executeQuery(
+                "SELECT userid FROM king " +
+                        "WHERE guildid = '" + guildId + "' AND channelid = '" + channelId + "'");
+
+        //If there's no hill in this channel + guild
+        if (!resultSet.next())
+            return;
+
+        //King vars part 1
+        int kingidIndex = resultSet.findColumn("userid");
+        String kingId = (String) resultSet.getObject(kingidIndex);
+
+        //If there already is row for this guild and channel, just no king
+        if (kingId == null) {
+            statement.execute("UPDATE king " +
+                    "SET userid = '" + userId + "' " +
+                    "WHERE guildid = '" + guildId + "' AND channelid = '" + channelId + "'");
+
+            //Distribute roles
+            distributeRoles(null, member, guild);
+
+            channel.sendMessage("**" + nickname + "** is now king of the hill!").queue();
+            return;
+        }
+
+        // If nobody is mentioned
+        if (mentionedUsers.isEmpty()) {
+            channel.sendMessage("Please mention a player to push!").queue();
+            return;
+        }
+
+
+
+        //King vars P2
+        String pushedUserId = mentionedUsers.get(0).getId();
+        User kingUser = Main.jda.retrieveUserById(kingId).complete();
+        Member kingMember = guild.retrieveMember(kingUser).complete();
+        String kingNickname;
+
+        //If you mention yourself
+        if (pushedUserId.equals(userId)) {
+            channel.sendMessage("You can't push yourself off, silly!").queue();
+            return;
+        }
+
+        //Get personal name for king
+        if (kingMember.getNickname() == null)
+            kingNickname = kingUser.getName();
+        else
+            kingNickname = kingMember.getNickname();
+
+        //Otherwise check if they pushed the king off
+        if (pushedUserId.equals(kingId)) {
+            statement.executeUpdate("UPDATE king " +
+                    "SET userid = '" + userId + "' " +
+                    "WHERE guildid = '" + guildId + "' AND channelid = '" + channelId + "'");
+
+            //Distribute roles
+            distributeRoles(kingMember, member, guild);
+
+            channel.sendMessage("**" + nickname + "** pushed **" + kingNickname + "** off the hill!").queue();
+            return;
+        }
+        //Else if they didn't push the king off the hill
+        else {
+            channel.sendMessage("Please push **" + kingNickname + "** off the hill!").queue();
+        }
+    }
+
+    public static void createCommand(Guild guild, Member member, TextChannel channel) throws SQLException {
+        //Vars
+        Statement statement = Main.statement;
+        String guildId = guild.getId();
+        String channelId = channel.getId();
+
+        //If they don't have manage server perms, ignore them
+        if (!member.hasPermission(Permission.MANAGE_SERVER))
+            return;
+
+        //Get user ID of king
+        ResultSet resultSet = statement.executeQuery(
+                "SELECT * FROM king " +
+                        "WHERE guildid = '" + guildId + "' AND channelid = '" + channelId + "'");
+
+        //Check if king already exists here
+        if (resultSet.next()) {
+            channel.sendMessage("Cannot create a hill here, hill already exists!").queue();
+            return;
+        }
+
+        //Check if roles already exist
+        List<Role> kothRole = guild.getRolesByName("King of the Hill!", false);
+        List<Role> pushedRole = guild.getRolesByName("Pushed off the Hill", false);
+        //Make it if it doesn't
+        if (kothRole.isEmpty()) {
+            guild.createRole()
+                    .setName("King of the Hill!")
+                    .setPermissions()
+                    .queue();
+        }
+        if (pushedRole.isEmpty()) {
+            guild.createRole()
+                    .setName("Pushed off the Hill")
+                    .setPermissions()
+                    .queue();
+        }
+
+        //Otherwise create a hill here
+        statement.execute("INSERT INTO king (guildid, channelid) VALUES " +
+                "('" + guildId + "', '" + channelId + "')");
+        channel.sendMessage("Hill created! Do `-push` to start!").queue();
+    }
+
+    /*
+    *** UTILITIES ***
+    */
+    public static void distributeRoles(Member kingMember, Member member, Guild guild) {
+        List<Role> kothRoles = guild.getRolesByName("King of the Hill!", false);
+        Role kothRole = kothRoles.get(0);
+        List<Role> pushedRoles = guild.getRolesByName("Pushed off the Hill", false);
+        Role pushedRole = pushedRoles.get(0);
+
+        //Remove pushed off role from pusher
+        guild.removeRoleFromMember(member, pushedRole).queue();
+        //Give king role to pusher
+        guild.addRoleToMember(member, kothRole).queue();
+
+        //In case we don't need to interact with king, return
+        if (kingMember == null)
+            return;
+        
+        //Remove king role from king
+        guild.removeRoleFromMember(kingMember, kothRole).queue();
+        //Give pushed off role to king
+        guild.addRoleToMember(kingMember, pushedRole).queue();
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,92 @@
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.*;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
+
+import java.sql.*;
+import java.util.List;
+
+public class Main extends ListenerAdapter {
+
+    static JDA jda;
+    static String token;
+    static String prefix;
+
+    //SQL
+    static Connection connection;
+    static Statement statement;
+
+    public static boolean startBot() throws InterruptedException {
+        JDABuilder preBuild = JDABuilder.createDefault(token);
+        preBuild.setActivity(Activity.playing("king of the hill!"));
+        try {
+            jda = preBuild.build();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        jda.addEventListener(new Main());
+        jda.awaitReady();
+        return true;
+    }
+
+    public static void main(String[] args) {
+        token = System.getenv("KOTH_BOT_TOKEN");
+        prefix = "-";
+
+        String url = System.getenv("KOTH_BOT_SQL_URL");
+        try {
+            connection = DriverManager.getConnection(url,System.getenv("KOTH_BOT_SQL_USERNAME"),System.getenv("KOTH_BOT_SQL_PASSWORD"));
+            statement = connection.createStatement();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        
+        try {
+            startBot();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Command executor
+    @Override
+    public void onGuildMessageReceived(@NotNull GuildMessageReceivedEvent event) {
+        Message message = event.getMessage();
+        String messageRaw = message.getContentRaw();
+        User user = event.getAuthor();
+        Member member = event.getMember();
+        Guild guild = event.getGuild();
+        TextChannel channel = event.getChannel();
+        List<User> mentionedUsers = message.getMentionedUsers();
+
+        if (!messageRaw.startsWith(prefix))
+            return;
+
+        String messageNoPrefix = messageRaw.substring(prefix.length());
+
+        switch (messageNoPrefix.split("\\s")[0]) {
+            case "p":
+            case "push": {
+                try {
+                    Commands.pushCommand(user, member, guild, channel, mentionedUsers);
+                } catch (SQLException throwables) {
+                    channel.sendMessage("Database error :(").queue();
+                    throwables.printStackTrace();
+                }
+                break;
+            }
+            case "c":
+            case "create":
+                try {
+                    Commands.createCommand(guild, member, channel);
+                } catch (SQLException throwables) {
+                    channel.sendMessage("Database error :(").queue();
+                    throwables.printStackTrace();
+                }
+                break;
+        }
+    }
+}
